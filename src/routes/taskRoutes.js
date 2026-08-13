8ffc21a820f3515b3dbb43ae9eeaad09f475d546
inesh-dev <ishanpokhrel16@gmail.com>
2026-08-13 21:03:16 +0545

    feat: add request validation middleware for task input

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const taskController = require('../controllers/taskController');
+const validateTaskInput = require('../middleware/validateTask');
 
 router.get('/', taskController.getAllTasks);
 router.get('/:id', taskController.getTaskById);
-router.post('/', taskController.createTask);
-router.put('/:id', taskController.updateTask);
+router.post('/', validateTaskInput, taskController.createTask);
+router.put('/:id', validateTaskInput, taskController.updateTask);
 router.delete('/:id', taskController.deleteTask);
 
 module.exports = router;
