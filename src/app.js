b48c6ec6c070c9fc1835d1f0fdd19f78bce10acf
inesh-dev <ishanpokhrel16@gmail.com>
2026-08-13 20:23:49 +0545

    feat: add task routes and mount /api/tasks in the app

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
+const taskRoutes = require('./routes/taskRoutes');
+
 const app = express();
 
-
 app.use(express.json());
-
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Team Task Manager API is running.' });
 });
 
-module.exports = app;
+app.use('/api/tasks', taskRoutes);
+
+module.exports = app
