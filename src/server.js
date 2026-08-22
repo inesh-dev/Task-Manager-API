a9cd3d7c3aaa903cc243be1e5f572c21761f93c1
Sangam-dev <parajulis295@gmail.com>
2026-08-22 12:08:50 +0545

    Fixed Port collisions

--- src/server.js
+++ src/server.js
@@ -2,6 +2,19 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+});
+
+server.on('error', (err) => {
+  if (err.code === 'EADDRINUSE') {
+    const newPort = PORT + 1;
+    console.warn(`Port ${PORT} is in use. Trying port ${newPort}...`);
+    app.listen(newPort, () => {
+      console.log(`Server running on http://localhost:${newPort}`);
+    });
+  } else {
+    console.error('Server error:', err);
+    process.exit(1);
+  }
 });
